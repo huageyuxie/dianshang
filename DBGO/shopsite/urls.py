3a33911083a84c994c4eba120045fc2fd64c939e
zhaojianing <1228853142@qq.com>
2018-10-18 09:54:06 +0800

    views/urls/user_over

---
+++ DBGO/shopsite/urls.py
@@ -0,0 +1,11 @@
+from django.conf.urls import url
+
+from . import views
+
+urlpatterns = [
+    url(r'^user_login/$',views.user_login,name='user_login'),
+    url(r'^user_loginout/$',views.user_loginout,name='user_loginout'),
+    url(r'^register/$',views.register,name='register'),
+
+
+]
--- DBGO/shopsite/views.py
+++ DBGO/shopsite/views.py
@@ -16,3 +16,5 @@ def register(request):
     pass
 
 
+
+
