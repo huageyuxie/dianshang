fd473e7ae71f06a6da775bf6a11765e57fedd29e
zhaojianing <1228853142@qq.com>
2018-10-18 21:17:45 +0800

    注册未完1.0

--- DBGO/shopsite/urls.py
+++ DBGO/shopsite/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     url(r'^index/$', views.index, name='index'),
     url(r'^user_login/$', views.user_login, name='user_login'),
     url(r'^user_logout/$', views.user_logout, name='user_logout'),
-    url(r'^register/$', views.user_register, name='user_register'),
+    url(r'^user_register/$', views.user_register, name='user_register'),
 
 
     url(r'^$', views.index)
