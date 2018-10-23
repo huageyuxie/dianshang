50fb3c77d0cff2a7a4dc8227ed7ecc3f68d323e8
ASoc9264 <1342903581@qq.com>
2018-10-23 14:43:15 +0800

    2018-10-23   13:56 商品界面2.0开始

--- DBGO/stores/models.py
+++ DBGO/stores/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+from shopsite.models import NormalUser
+
+
+class Store(models.Model):
+    id = models.AutoField(primary_key=True)
+    # 商店名称
+    name = models.CharField(max_length=50, verbose_name="店铺名称")
+    # 店铺图片
+    cover = models.ImageField(upload_to='static/images/stores/default/default.jpg')
+    # 店铺介绍
+    intro = models.CharField(max_length=255, verbose_name="店铺介绍")
+    # 店铺状态 0:关闭 1:正常营业 2:休息
+    status = models.IntegerField(default=0, verbose_name="店铺状态")
+    # 店铺添加时间
+    add_time = models.DateTimeField(auto_now_add=True, verbose_name="店铺添加时间")
+
+    # 店铺所属用户
+    user = models.ForeignKey(NormalUser, on_delete=models.CASCADE, verbose_name="店铺店长")
+
