ce61fae23261c98f75636b718f5d4a4e6a103c71
ASoc9264 <1342903581@qq.com>
2018-10-23 15:22:09 +0800

    2018-10-23   15:21 开店1.0

---
+++ DBGO/stores/urls.py
@@ -0,0 +1,11 @@
+from django.conf.urls import url
+
+
+from . import views
+urlpatterns = [
+    url(r'add_store/$', views.add_store, name='add_store'),
+    url(r'del_store/$', views.del_store, name='del_store'),
+    url(r'update_store/$', views.update_store, name='update_store'),
+    url(r'open_store/$', views.open_store, name='open_store'),
+    url(r'close_store/$', views.close_store, name='close_store'),
+]
--- DBGO/stores/views.py
+++ DBGO/stores/views.py
@@ -1,3 +1,90 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
+# 开店
+from . import models
+
+
+def add_store(request):
+    """
+    创建店铺
+    :param request:
+    :return:
+    """
+    if request.method == "GET":
+        return render(request, 'store/add_store1.html', {'msg': '请填写以下数据'})
+    if request.method == "POST":
+        name = request.POST['name']
+        intro = request.POST['intro']
+        user = request.user.normaluser
+        store = models.Store(name=name, intro=intro, user=user)
+        cover = request.FILES.get('cover', False)
+        if cover:
+            store.cover = cover
+        store.save()
+        return render(request, 'store/index.html', {'store': store})
+
+
+# 删除店铺
+def del_store(request, store_id):
+    """
+    删除店铺
+    :param request:
+    :return:
+    """
+    store = models.Store.objects.get(id=store_id)
+    store.status = 0
+    store.save()
+    return redirect('/shopsite/index/')
+
+
+# 修改店铺
+def update_store(request, store_id):
+    """
+    修改店铺的信息
+    :param request:
+    :param store_id:
+    :return:
+    """
+    if request.method == 'GET':
+        return render(request, 'store/update_html')
+    if request.method == "POST":
+        name = request.POST['name']
+        intro = request.POST['intro']
+        user = request.user.normaluser
+        store = models.Store.objects.get(id=store_id)
+        store.name = name
+        store.intro = intro
+        store.user = user
+        cover = request.FILES.get('cover', False)
+        if cover:
+            store.cover = cover
+        store.save()
+        return render(request, 'store/index.html', {'msg': "店铺信息修改成功", 'store': store})
+
+
+# 店铺营业
+def open_store(request, store_id):
+    """
+    店铺开始营业
+    :param request:
+    :return:
+    """
+    store = models.Store.objects.get(id=store_id)
+    store.status = 1
+    store.save()
+    return render(request, 'store/index.html', {'msg': '店铺开始营业了', 'store': store})
+
+
+# 店铺歇业
+def close_store(request, store_id):
+    """
+    店铺展示歇业
+    :param request:
+    :param store_id:
+    :return:
+    """
+    store = models.Store.objects.get(id=store_id)
+    store.status = 2
+    store.save()
+    return render(request, 'store/index.html', {'msg': '店铺暂时歇业了', 'store': store})
